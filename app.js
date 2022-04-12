6a0e565d9ed10e8b2f8187e0c7644838e467e795
mamenendez <mamenendez@gmail.com>
2022-04-11 22:47:03 -0300

    modulo 10

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const autos = require("./autos.js");
-const { autosQuePuedeComprar } = require("./concesionaria.js");
-const con = require("./concesionaria.js");
-const persona = require("./persona.js");
+// const autos = require("./autos.js");
+// const { autosQuePuedeComprar } = require("./concesionaria.js");
+// const con = require("./concesionaria.js");
+// const persona = require("./persona.js");
 
 //console.log(autos)
 
@@ -24,5 +24,46 @@ const persona = require("./persona.js");
 // const unAuto = con.buscarAuto("APL123");
 // console.log(con.puedeComprar(unAuto,unaPersona));
 
- const unaPersona =  persona;
- console.log(con.autosQuePuedeComprar(persona));
+//  const unaPersona =  persona;
+//  console.log(con.autosQuePuedeComprar(persona));
+
+//Servidor.
+const express = require('express');
+const path= require('path');
+
+const app = express();
+
+
+app.listen(3000,() => {
+    console.log("Servidor corriendo");
+});
+
+app.get('/',function(req,res)
+{
+    res.sendFile(path.resolve(__dirname,'./views/index.html'));
+});
+
+
+app.get('/contacto',function(req,res)
+{
+    res.send([1,2,3,'Tute', "Hola"]);
+} );
+
+app.get('/un-objeto', function(req, res)
+{
+    res.send({
+            Name: "Matias",
+            Apellido: "Menendez"
+        });
+} );
+
+
+app.get('/about-us', function (req,res) {
+    let pathCompleto = path.join(__dirname,'./views/about-us.html');
+    res.sendFile(pathCompleto);
+});
+
+
+
+
+
